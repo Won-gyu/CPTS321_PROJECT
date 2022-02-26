71a9f7e710c369ae7cb9dff8812ec9da037c0e3e
wongyuh <wongyuh36@gmail.com>
2022-02-25 18:32:06 -0800

    Add PooledSoundPlaye

---
+++ CPTS321_PROJECT/CPTS321_PROJECT/Src/PooledSoundPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPTS321_PROJECT.Src
+{
+    public class PooledSoundPlayer
+    {
+        private SoundPlayer soundPlayer;
+        private float lifespan;
+        private bool isAvailable;
+        public bool IsAvailable
+        {
+            get
+            {
+                return isAvailable;
+            }
+        }
+
+        public PooledSoundPlayer(string name, float lifespan)
+        {
+            this.soundPlayer = new SoundPlayer(name);
+            this.lifespan = lifespan;
+        }
+
+        public void Play()
+        {
+            soundPlayer.Play();
+            isAvailable = false;
+            Task.Delay((int)(lifespan * 1000)).ContinueWith(t => { isAvailable = true; });
+        }
+    }
+}
